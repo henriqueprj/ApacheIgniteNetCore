dbf7885e3e8246b558c1092536a4035488203bfc
Henrique Ribeiro <henriqueprj@gmail.com>
2020-09-17 18:38:00 -0300

    feat: balance

--- ApacheIgniteExample.Domain/UseCases/Balance/GetBalance.cs
+++ ApacheIgniteExample.Domain/UseCases/Balance/GetBalance.cs
@@ -2,7 +2,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace ApacheIgniteExample.Domain.Commands.Balance
+namespace ApacheIgniteExample.Domain.UseCases.Balance
 {
     public class GetBalance
     {
